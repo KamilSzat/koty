bc6c19b1e04b9e1bb746002717f080c311da8f3a
KamilSzat <uT_3776@zsp3zamosc.pl>
2024-11-06 09:23:37 +0100

    Wyszukiwanie kotów

---
+++ MainPage.xaml.cs
@@ -0,0 +1,48 @@
+﻿using Windows.Web.Http.Headers;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace kot
+{
+    public partial class MainPage : ContentPage
+    {
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            imgkot.Source = new UriImageSource
+            {
+                Uri = new Uri("https://cataas.com/cat")
+
+            };
+        }
+        private void OnCounterClicked2(object sender, EventArgs e)
+        {
+            imgkot_gift.Source = new UriImageSource
+            {
+                Uri = new Uri("https://cataas.com/cat/gif")
+            };
+        }
+        private void OnCounterClicked3(object sender, EventArgs e)
+        {
+            string kot_size = kotki1.Text;
+            string kot_text = kotki.Text;
+
+            imgkot_text.Source = new UriImageSource
+            {
+                Uri = new Uri("https://cataas.com/cat/says/" + kot_text + "?fontSize=" + kot_size)
+            };
+        }
+        private void OnCounterClicked4(object sender, EventArgs e)
+        {
+            imgkot_filtr.Source = new UriImageSource
+            {
+                Uri = new Uri("https://cataas.com/cat?filter=mono")
+            };
+
+        }
+    }
+}
